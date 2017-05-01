e9cb400c9665e97db18cb4cdfeb1dbf53a21d1a6
Reggie Zhang <reggiezhang@users.noreply.github.com>
2017-05-01 10:31:35 +0800

    Update move-mails-outlook-mac.js

--- move-mails-outlook-mac.js
+++ move-mails-outlook-mac.js
@@ -1,26 +1,38 @@
 #!/usr/bin/env osascript -l JavaScript
 
 "use strict";
-var outlook = Application("Microsoft Outlook");
 
-// Entry point
-function run(argv) {
-    if (argv.length < 2) {
-        return "usage: move-mails-outlook-mac.js <email> <folder>";
+var Outlook = (function () {
+    var instance;
+    function init() {
+        return Application("Microsoft Outlook");
     }
-    var destAccount = getDestAccount(argv[0]);
-    checkNull(destAccount, "Cannot found dest account: " + argv[0]);
-    var destFolder = getDestFolder(destAccount, argv[1]);
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = init();
+            }
+            return instance;
+        }
+    };
+})();
 
-    var srcAccount = outlook.defaultAccount();
-    var srcFolder = srcAccount.inbox();
+function archiveFolder(srcAccountName, srcFolder, destAccountName, destFolder) {
+    console.log("Archiving messages from %s/%s to %s/%s", srcAccountName, srcFolder.name(), destAccountName, destFolder.name());
+    doArchiving(srcFolder, destFolder);
+}
 
-    console.log("Archiving messages from " + srcAccount.name() + "/Inbox to " + destAccount.name() + "/" + destFolder.name());
-    archiveFolder(srcFolder, destFolder);
+function getMailFolderClass() {
+    return Outlook.getInstance().defaultAccount().inbox().class();
 }
 
 function getParentFolderName(folder) {
-    return folder.container().name() != null ? "/" + folder.container().name() : "";
+    var parentFolderName = "";
+    while (folder.container().name() != null) {
+        parentFolderName = "/" + folder.container().name() + parentFolderName;
+        folder = folder.container();
+    }
+    return parentFolderName;
 }
 
 function findSubFolderByName(folder, name) {
@@ -35,7 +47,7 @@ function getSubFolderByName(folder, name) {
 }
 
 function makeMailFolder(parentFolder, folderName) {
-    return outlook.make({
+    return Outlook.getInstance().make({
         new: parentFolder.class(), at: parentFolder, withProperties: { name: folderName }
     });
 }
@@ -47,12 +59,12 @@ function msgCntInFolderForArchiving(folder) {
 function moveMsgs(srcFolder, destFolder) {
     srcFolder.messages().forEach(function (msg) {
         if (msg.isRead()) {
-            outlook.move(msg, { to: destFolder });
+            Outlook.getInstance().move(msg, { to: destFolder });
         }
     });
 }
 
-function archiveFolder(srcFolder, destFolder) {
+function doArchiving(srcFolder, destFolder) {
     var msgCnt = msgCntInFolderForArchiving(srcFolder);
     var parentName = getParentFolderName(srcFolder);
     console.log("Archiving: %s/%s (%d)", parentName, srcFolder.name(), msgCnt);
@@ -61,7 +73,7 @@ function archiveFolder(srcFolder, destFolder) {
     srcFolder.mailFolders().forEach(function (srcSubFolder) {
         var destSubFolder = getSubFolderByName(destFolder, srcSubFolder.name());
         if (destSubFolder != null) {
-            archiveFolder(srcSubFolder, destSubFolder);
+            doArchiving(srcSubFolder, destSubFolder);
         } else {
             var msgCnt = msgCntInFolderForArchiving(srcSubFolder);
             console.log("Skipping : /%s/%s (%d)", srcFolder.name(), srcSubFolder.name(), msgCnt);
@@ -70,7 +82,7 @@ function archiveFolder(srcFolder, destFolder) {
 }
 
 function getDestAccount(email) {
-    return outlook.exchangeAccounts().find(function (elem) {
+    return Outlook.getInstance().exchangeAccounts().find(function (elem) {
         return elem.emailAddress() == email;
     });
 }
@@ -83,13 +95,30 @@ function findDestFolder(destAccount, folderName) {
 
 function getDestFolder(destAccount, folderName) {
     var destFolder = findDestFolder(destAccount, folderName);
-    return destFolder != null ? destFolder : outlook.make({
-            new: destAccount.inbox().class(), at: destAccount, withProperties: { name: folderName }
-        });
+    return destFolder != null ? destFolder : Outlook.getInstance().make({
+        new: destAccount.inbox().class(), at: destAccount, withProperties: { name: folderName }
+    });
 }
 
 function checkNull(obj, errorDesc) {
     if (!obj) {
         throw errorDesc;
     }
+}
+
+// Entry point
+function run(argv) {
+    if (argv.length < 2) {
+        return "usage: move-mails-outlook-mac.js <email> <folder>";
+    }
+    var destAccount = getDestAccount(argv[0]);
+    checkNull(destAccount, "Cannot found dest account: " + argv[0]);
+    var destFolder = getDestFolder(destAccount, argv[1]);
+
+    var srcAccount = Outlook.getInstance().defaultAccount();
+    var srcInboxFolder = srcAccount.inbox();
+    var srcSentFolder = srcAccount.sentItems();
+
+    archiveFolder(srcAccount.name(), srcInboxFolder, destAccount.name(), destFolder);
+    archiveFolder(srcAccount.name(), srcSentFolder, destAccount.name(), destFolder);
 }
